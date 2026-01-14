130d68e68518cf644cd02a8c764df57e04636169
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-14 20:08:14 +0200

    3. Komponendi sisustamine andmetega

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,16 @@
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+function ExpenseItem(props){
     return (
-    <div className='expense-item'>
-        <div>Date</div>
-        <div className='expense-item__description'>
-            <h2>Title</h2>
-            <div className='expense-item__price'>Price</div>
+        <div className='expense-item'>
+            <div>{props.data.date.toString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.data.title}</h2>
+                <div className='expense-item__price'>
+                    {props.data.price}
+                </div>
+            </div>
         </div>
-    </div>
     )
 }
 
