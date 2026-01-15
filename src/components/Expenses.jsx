266188e29c5c8d4c81debe97a0b25e70e93015d2
olha <189846437+sibaraku@users.noreply.github.com>
2026-01-15 14:42:07 +0200

    7. Komponentide kompositsioon ja info edastamine

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,14 +1,17 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem.jsx';
+import Card from './Card.jsx';
 
 function Expenses(props) {
     console.log(props);
   return (
-    <div className="expenses">
-      {props.data.map((expense) => (
-        <ExpenseItem key={expense.id} data={expense} />
-      ))}
-    </div>
+    <Card>
+      <div className="expenses">
+        {props.data.map((expense) => (
+          <ExpenseItem key={expense.id} data={expense} />
+        ))}
+      </div>
+    </Card>
   );
 }
 
