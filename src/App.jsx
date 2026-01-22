7d092c7ec09ae688a329283890f2675264d452fb
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-22 18:17:16 +0200

    8. 11. Vormitöötlus Reactis

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,14 @@ function App() {
     }
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div className="App">
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses data={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -2,36 +2,57 @@ import './ExpenseForm.css';
 import { useState } from 'react';
 
 const ExpenseForm = (props) => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredPrice, setEnteredPrice] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
-
+    const[userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredPrice: '',
+        enteredDate: ''
+    });
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-    }
-
+        setUserInput({
+            ...userInput, enteredTitle: event.target.value
+        });
+    };
     const priceChangeHandler = (event) => {
-        setEnteredPrice(event.target.value);
-    }
-
+        setUserInput({
+            ...userInput, enteredPrice: event.target.value
+        });
+    };
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-    }
+        setUserInput({
+            ...userInput, enteredDate: event.target.value
+        });
+    };
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: userInput.enteredTitle,
+            amount: userInput.enteredPrice,
+            date: new Date(userInput.enteredDate)
+        };
+        props.onSaveExpenseData(expenseData);
+
+        setUserInput({
+            enteredTitle: '',
+            enteredPrice: '',
+            enteredDate: ''
+        });
+    };
+
 
   return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input type='text' onChange={titleChangeHandler} value={userInput.enteredTitle} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Price</label>
-                    <input type='number' min='0.01' step='0.01' onChange={priceChangeHandler} />
+                    <input type='number' min='0.01' step='0.01' onChange={priceChangeHandler} value={userInput.enteredPrice} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2025-01-01' max='2026-12-31' onChange={dateChangeHandler} />
+                    <input type='date' min='2024-01-01' max='2026-12-31' onChange={dateChangeHandler} value={userInput.enteredDate} />
                 </div>
             </div>
             <div className='new-expense__actions'>
