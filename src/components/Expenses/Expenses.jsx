2e92b24bfb81026de312e51506ebca49356853c2
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-25 18:03:35 +0200

    12. 4. Andmete suunamine ja väärtuse hetkeseisu lugemine ning määramine

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,20 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem.jsx';
 import Card from '../UI/Card.jsx';
+import ExpensesFilter from './ExpensesFilter.jsx';
+import { useState } from 'react';
 
 function Expenses(props) {
-    console.log(props);
+    const [filteredYear, setFilteredYear] = useState('2023');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        console.log("Year data is Expenses.jsx ", selectedYear);
+    }
+    
   return (
     <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         {props.data.map((expense) => (
           <ExpenseItem key={expense.id} data={expense} />
         ))}
