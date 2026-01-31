34cf8f04efb06af4650fb61a9a1b1a5d2064a327
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-31 21:04:06 +0200

    Ül.6 - vormi kuvamine ja asendamine nuppuga

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,21 +1,40 @@
 import ExpenseForm from './ExpenseForm.jsx';
 import './NewExpense.css';
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+
+    const [editForm, setEditForm] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setEditForm(false);
+    };
+
+    const startEditHandler = () => {        
+        setEditForm(true);
+        console.log("form edit open");
+    };
+
+    const stopEditHandler = () => {        
+        setEditForm(false);
+        console.log("form edit close");
     };
 
   return(
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        {!editForm && (<button onClick={startEditHandler}>Add New Expense</button>)}
+
+        {editForm && (<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditHandler} />)}
         </div>
 
     );
 }
 
 export default NewExpense;
+
+
